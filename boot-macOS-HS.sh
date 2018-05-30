e85a88c244530cdef64196b4177e3c52a4b5bffc
Giovanni <sinkingsugar@gmail.com>
2018-05-30 12:46:13 +0900

    Boot with pci-pass

--- boot-macOS-HS.sh
+++ boot-macOS-HS.sh
@@ -13,10 +13,11 @@
 
 MY_OPTIONS="+aes,+xsave,+avx,+xsaveopt,avx2,+smep"
 
-qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
+qemu-system-x86_64 -enable-kvm -m 14336 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
 	  -machine pc-q35-2.9 \
 	  -smp 4,cores=2 \
 	  -usb -device usb-kbd -device usb-tablet \
+	  -device usb-host,vendorid=0x05ac,productid=0x020b -device usb-host,vendorid=0x045e,productid=0x0797 \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
 	  -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd \
 	  -drive if=pflash,format=raw,file=OVMF_VARS-1024x768.fd \
@@ -26,7 +27,9 @@ qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+i
 	  -drive id=Clover,if=none,snapshot=on,format=qcow2,file=./'Clover.qcow2' \
 	  -device ide-drive,bus=ide.1,drive=MacHDD \
 	  -drive id=MacHDD,if=none,file=./mac_hdd.img,format=qcow2 \
-	  -device ide-drive,bus=ide.0,drive=MacDVD \
-	  -drive id=MacDVD,if=none,snapshot=on,media=cdrom,file=./'HighSierra-10.13.4.iso' \
+	  -vga none -device vfio-pci,host=03:00.0,bus=pcie.0,multifunction=on -device vfio-pci,host=03:00.1,bus=pcie.0 \
 	  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
 	  -monitor stdio
+
+# -device ide-drive,bus=ide.0,drive=MacDVD \
+# -drive id=MacDVD,if=none,snapshot=on,media=cdrom,file=/mnt/data0/ISOs/HighSierra.iso \
